e8054ecdc2aad7091d031ea351c889a487ec02c6
Elina Kalnina <elinak79@gmail.com>
2020-07-09 03:13:26 +0300

    Initial Commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def create
+    @tasks = Task.find(params[:task_id])
+    @comment = @tasks.comments.create(comment_params)
+    redirect_to tasks_path(@tasks)
+
+  end
+  def destroy
+    @tasks = Task.find(params[:task_id])
+    @comment = @tasks.comments.find(params[:id])
+    @comment.destroy
+    redirect_to tasks_path(@tasks)
+  end
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+
+end
